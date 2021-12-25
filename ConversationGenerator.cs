cb955a74ce1ddf420618cc3a4eea9ef5d2060939
CarlbertCannon <providenceneedschange@gmail.com>
2021-12-25 11:02:13 -0700

    inital changes

---
+++ ConversationGenerator.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanyeWest
+{
+    public class ConversationGenerator
+    {
+        public static void KanyeQuote()
+        {
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var KanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"--------");
+            Console.WriteLine($"Kayne: '{KanyeQuote}' ");
+            Console.WriteLine($"--------");
+        }
+
+        public static void RonQuote()
+        {
+            var client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronsResponse = client.GetStringAsync(ronURL).Result;
+
+            var RonQuote = JArray.Parse(ronsResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine($"--------");
+            Console.WriteLine($"Ron: '{RonQuote}' ");
+            Console.WriteLine($"--------");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using KanyeWest;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeREST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConversationGenerator.KanyeQuote();
+
+            ConversationGenerator.RonQuote();
+
+        }
+    }
+}
